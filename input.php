50862c22959517684b074eb64d9414ee1ae53e01
suraphol-macbook <iteekung@gmail.com>
2017-01-15 15:48:03 +0700

    add input file and printslipt

---
+++ input.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset="utf-8">
+      <title>Parkko</title>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+   </head>
+   <body>
+      <div class="container">
+         <header>
+            <h1>Input</h1>
+         </header>
+         <div class="row">
+            <div class="form-group">
+               <label>ทะเบียนรถ</label>
+               <input type="text" class="form-control" name="" value="">
+            </div>
+         </div>
+         <div class="row">
+            <div class="form-group">
+               <label>จังหวัด</label>
+               <input type="text" class="form-control" name="provine" value="">
+            </div>
+         </div>
+         <div class="row">
+            <input type="submit" class="btn btn-success" value="submit">
+         </div>
+      </div>
+   </body>
+</html>
---
+++ printslipt.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset="utf-8">
+      <title>Parkko</title>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+   </head>
+   <body>
+      <div class="container">
+         <header>
+            <h1>รายละเอียด</h1>
+         </header>
+         <div class="row">
+            <div class="col-sm-3">
+               ทะเบียน
+            </div>
+            <div class="col-sm-9">
+               สฬ 5420
+            </div>
+         </div>
+         <div class="row">
+            <div class="col-sm-3">
+               จังหวัด
+            </div>
+            <div class="col-sm-9">
+               กทม.
+            </div>
+         </div>
+      </div>
+   </body>
+</html>
