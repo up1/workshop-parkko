fae2b9bdaa2c0ce5dc26cef041521399d4fd1c64
Pichid Detson <bencomtech@gmail.com>
2017-01-15 16:26:07 +0700

    phpunit test

--- classes/Calculate.php
+++ classes/Calculate.php
@@ -2,8 +2,19 @@
 
 class Calculate
 {
-	public function getChange()
+	public function getChange($total)
 	{
-		return null;
+		$bank500 = floor($total/500);
+		$bank100 = floor($total%500/100);
+		$bank50 = floor($total%500%100/50);
+		$bank20 = floor($total%500%100%50/20);
+		$bank10 = floor($total%500%100%50%20/10);
+		$result_change = array(
+			"bank500" => $bank500
+			,"bank100" => $bank100
+			,"bank50" => $bank50
+			,"bank20" => $bank20
+			,"bank10" => $bank10);
+		return $result_change;
 	}
 }
--- tests/CalculateTest.php
+++ tests/CalculateTest.php
@@ -1,13 +1,27 @@
 <?php
+require_once __DIR__ . "/../vendor/autoload.php";
 require_once __DIR__ . "/../classes/Calculate.php";
 
 use PHPUnit\Framework\TestCase;
 
 class CalculateTest extends TestCase
 {
-	public function test()
+	public function test_total_40_receive_50_change_10()
 	{
 		$cal = new Calculate();
-		$this->assertEquals(null, $cal->getChange());
+		$res = $cal->getChange(10);
+		$this->assertInternalType('array', $res);
+		$this->assertEquals(5, count($res));
+		$res_1 = $res["bank500"];
+		$res_2 = $res["bank100"];
+		$res_3 = $res["bank50"];
+		$res_4 = $res["bank20"];
+		$res_5 = $res["bank10"];
+
+		$this->assertEquals(0, $res_1);
+		$this->assertEquals(0, $res_2);
+		$this->assertEquals(0, $res_3);
+		$this->assertEquals(0, $res_4);
+		$this->assertEquals(1, $res_5);
 	}
 }
